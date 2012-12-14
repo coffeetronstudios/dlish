23588422eb577fd1c5d865b8512b72a2336ac45b
Jake Petroules <jake.petroules@petroules.com>
2012-12-13 19:08:50 -0500

    Fix cart box scroll offset.

--- scripts/application.js
+++ scripts/application.js
@@ -1,7 +1,10 @@
 !function ($) {
     "use strict";
 
-    $('[data-spy="affix"]').each(function () {
-        $(this).affix();
+    $('.cart-box').each(function() {
+        var el = $(this);
+        el.affix({
+            offset: el.position().top - 100
+        });
     });
 }(window.jQuery);
