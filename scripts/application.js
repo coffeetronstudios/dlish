207d54ad20bef699b839b9dcf5ffa2ede1f7afa5
Jake Petroules <jake.petroules@petroules.com>
2012-12-20 21:20:15 -0500

    Make secondary navigation bar show/hide on hover/leave.

--- scripts/application.js
+++ scripts/application.js
@@ -1,10 +1,24 @@
 !function ($) {
     "use strict";
 
-    $('.cart-box').each(function() {
-        var el = $(this);
-        el.affix({
-            offset: el.position().top - 100
+    $(function() {
+        $('.cart-box').each(function() {
+            var el = $(this);
+            el.affix({
+                offset: el.position().top - 100
+            });
+        });
+
+        var nav = $('.secondary-navigation.toffee');
+
+        $('.toffe').each(function() {
+            $(this).click(function() {
+                nav.slideDown();
+            });
+        });
+
+        nav.mouseout(function() {
+            nav.slideUp();
         });
     });
 }(window.jQuery);
